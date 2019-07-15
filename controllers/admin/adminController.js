88c3643f063a7c2d6f201ba71dd82a41bbcecafa
Wade-Martin <wsmartin23@gmail.com>
2019-07-15 12:14:06 +1000

    create business logic added and tested in postman

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -1,10 +1,23 @@
+const Business = require('../../models/Business')
 // private endpoints go here
 const currentUser = (req, res) => {
   //current admin user logic goes here
+}
+
+const createBusiness = async (req, res) => {
+  const { businessID, businessName, abn } = req.body
+  try {
+    const newBus = await new Business({businessID, businessName, abn})
+    const savedBus = await newBus.save();
+    console.log(savedBus)
+    res.status(200).send(savedBus)
+  } catch (error) {
+    res.status(503).send(error.message)
+  }
 }
 
 const login = async (req, res) => {
   console.log("Admin Login:", req.body);
 };
 
-module.exports = {currentUser, login};
+module.exports = {currentUser, login, createBusiness};
