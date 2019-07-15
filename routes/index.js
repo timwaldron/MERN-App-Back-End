7b6035a72f2d7186a8d350e84644bc6825c1d86d
Timothy Waldron <tim@waldron.im>
2019-07-15 13:34:21 +1000

    Added cors origin

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // middleware
 router.use(express.json())
-router.use(cors());
+router.use(cors({ origin: 'https://back-end-timwaldron.mern-project.now.sh' }));
 
 // routing
 router.use('/admin', require('./adminRoutes'));
